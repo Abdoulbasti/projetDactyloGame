0f28a46619ec316e6357e270abdb468aa1d17ff8
Abdoulbasti <mukailaabdoulbassit@gmail.com>
2022-12-20 22:25:21 +0100

    Interface graphique en cours...

--- src/main/java/interface_graphique/Menu_principale.java
+++ src/main/java/interface_graphique/Menu_principale.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import constantes.Pargraphes;
 
@@ -21,13 +22,18 @@ public class Menu_principale extends Application{
         //Color couleur = Color.GREY;
         int espace = 100;
         String nom_fenetre_princile = "DACTYLO-GAME";
-        VBox parent = new VBox(espace);
+        //VBox parent = new VBox();
+        Group parent = new Group();
 
         Label text_affiche = new Label(Pargraphes.PARAGRAPHE_1);
         TextArea saisie_utilisateur = new TextArea();
+        emplacement_champs_texte(saisie_utilisateur);
 
+        //parent.getChildren().addAll(text_affiche, saisie_utilisateur);
         parent.getChildren().addAll(text_affiche, saisie_utilisateur);
-        gestion_emplacement_enfants(parent);
+        //gestion_emplacement_enfants(parent);
+
+        //emplacement_champs_texte(saisie_utilisateur);
 
 
 
@@ -35,7 +41,6 @@ public class Menu_principale extends Application{
         fenetre.setTitle(nom_fenetre_princile);
         this.logo_jeu(fenetre);
         caracteristiques_fenetres(fenetre);
-
         fenetre.setScene(scene);
         fenetre.show();
     }
@@ -57,6 +62,21 @@ public class Menu_principale extends Application{
         fenetre.centerOnScreen();
     }
 
+    public void emplacement_champs_texte(TextArea champs_text)
+    {
+        //champs_text.resize(50, 50);
+        //champs_text.setPrefWidth(0);
+        //champs_text.setPrefHeight(500);
+
+        //champs_text.setPrefSize(23, 89);
+        //champs_text.setPrefHeight(120);
+        //champs_text.setPrefWidth(300);
+
+        champs_text.setLayoutX(50);
+        champs_text.setLayoutY(50);
+        champs_text.setWrapText(true);
+        champs_text.setFont(Font.font(20));
+    }
     public void gestion_emplacement_enfants(VBox elements)
     {
         elements.setAlignment(Pos.CENTER);
